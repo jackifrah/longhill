3481bb594d01a2a671e82d158d7d4c0a4b214e65
jackifrah <39552416-jackifrah@users.noreply.replit.com>
2025-02-26 17:11:24 +0000

    Update Footer: Remove institutional models link and reduce copyright text size.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a461c9f8-2e77-4090-93a6-aad56adb43e3/6cf320d9-5f51-48cd-9a6d-aa7388c529dd.jpg

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -7,16 +7,12 @@ export default function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2 md:px-0">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="text-sm leading-loose text-muted-foreground">
+            <span className="text-xs leading-loose text-muted-foreground">
               © 2024 Longhill Management. All rights reserved.
             </span>
           </Link>
         </div>
         <nav className="flex gap-4 md:gap-6">
-          <Link href="#" className="text-sm font-medium hover:underline">
-            Institutional-grade models for sale
-          </Link>
-          <Separator orientation="vertical" className="hidden h-6 md:block" />
           <a 
             href="https://app.usemotion.com/meet/jack-ifrah/meeting"
             target="_blank"
