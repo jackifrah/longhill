09b1ab763f2bd58984f73210bd0b1452a929ff3b
jackifrah <39552416-jackifrah@users.noreply.replit.com>
2025-02-26 15:56:28 +0000

    Add SVG logo to client application.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a461c9f8-2e77-4090-93a6-aad56adb43e3/4c6b5414-07cb-4a82-acee-41445dbdb7c7.jpg

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Logo(props: React.HTMLAttributes<HTMLImageElement>) {
   return (
     <img
-      src="/orly-logo-icon@2x-1.svg"
+      src="orly-logo-icon@2x-1.svg"
       alt="Longhill Consulting Logo"
       className="h-6 w-6 grayscale"
       {...props}
