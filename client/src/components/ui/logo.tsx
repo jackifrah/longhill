fc9ade7fb5a9c9833e5142969f9d0568557effcc
jackifrah <39552416-jackifrah@users.noreply.replit.com>
2025-02-26 16:01:16 +0000

    Adjust website typography and logo size.  Reduced body text font size and boldness, increased logo size.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a461c9f8-2e77-4090-93a6-aad56adb43e3/4ce42639-fb91-4c3b-b723-d0640380ed13.jpg

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -5,7 +5,7 @@ export function Logo(props: React.HTMLAttributes<HTMLImageElement>) {
     <img
       src="orly-logo-icon@2x-1.svg"
       alt="Longhill Consulting Logo"
-      className="h-6 w-6 grayscale"
+      className="h-8 w-8 grayscale"
       {...props}
     />
   );
