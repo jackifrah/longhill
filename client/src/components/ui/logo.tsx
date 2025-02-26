da1e7ac393db752309de1768860e243679c7ef18
jackifrah <39552416-jackifrah@users.noreply.replit.com>
2025-02-26 15:48:13 +0000

    Update logo component to use new SVG logo.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a461c9f8-2e77-4090-93a6-aad56adb43e3/0d4b68ea-fc4f-46b1-93ea-7f61170a160c.jpg

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -2,19 +2,11 @@ import { SVGProps } from "react";
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <img
+      src="/orly-logo-icon@2x-1.svg"
+      alt="Longhill Consulting Logo"
+      className="h-6 w-6"
       {...props}
-    >
-      <rect width="32" height="32" rx="16" fill="currentColor" />
-      <path
-        d="M16 8C11.582 8 8 11.582 8 16C8 20.418 11.582 24 16 24C20.418 24 24 20.418 24 16C24 11.582 20.418 8 16 8ZM16 22C12.686 22 10 19.314 10 16C10 12.686 12.686 10 16 10C19.314 10 22 12.686 22 16C22 19.314 19.314 22 16 22Z"
-        fill="white"
-      />
-    </svg>
+    />
   );
 }
