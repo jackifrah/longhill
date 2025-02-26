d7b94a933889cb556b0eb243e0aa82bd411bd898
jackifrah <39552416-jackifrah@users.noreply.replit.com>
2025-02-26 16:28:21 +0000

    Fix: Enhance Orly logo color visibility.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a461c9f8-2e77-4090-93a6-aad56adb43e3/008a937b-6f26-4500-8e7e-3a9c2db40830.jpg

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -5,7 +5,7 @@ export function Logo(props: React.HTMLAttributes<HTMLImageElement>) {
     <img
       src="orly-logo-icon@2x-1.svg"
       alt="Longhill Consulting Logo"
-      className="h-8 w-8 grayscale"
+      className="h-8 w-8" 
       {...props}
     />
   );
